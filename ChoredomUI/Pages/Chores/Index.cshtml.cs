c9d5e876cb7d2caf643c728b8b1b214227d275e5
megarney <megarney0@gmail.com>
2023-03-26 18:10:50 -0500

    chorelist cs done

--- ChoredomUI/Pages/Chores/Index.cshtml.cs
+++ ChoredomUI/Pages/Chores/Index.cshtml.cs
@@ -9,7 +9,23 @@ namespace ChoredomUI.Pages.Chores
         public List<Chore> ChoreList { get; set; } = new List<Chore>();
         public void OnGet()
         {
-
+            using (SqlConnection conn = new SqlConnection(DBHelper.GetConnectionString()))
+            {
+                string sql = "SELECT * FROM Chore Order By ChoreName";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                conn.Open();
+                SqlDataReadter reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while(reader.Read())
+                    {
+                        Chore chore = new Chore();
+                        chore.ChoreName = reader["ChoreName"].ToString();
+                        chore.ChoreId = int.Parse["ChoreId"].ToString();
+                        ChoreList.Add(chore);
+                    }
+                }
+            }
         }
     }
 }
