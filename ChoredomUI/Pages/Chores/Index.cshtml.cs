3b2668b7d15a3549d22fd5d12600d09d6fac3029
megarney <megarney0@gmail.com>
2023-03-27 09:42:58 -0500

    added sql

--- ChoredomUI/Pages/Chores/Index.cshtml.cs
+++ ChoredomUI/Pages/Chores/Index.cshtml.cs
@@ -16,14 +16,14 @@ namespace ChoredomUI.Pages.Chores
                 string sql = "SELECT * FROM Chore Order By ChoreName";
                 SqlCommand cmd = new SqlCommand(sql, conn);
                 conn.Open();
-                SqlDataReadter reader = cmd.ExecuteReader();
+                SqlDataReader reader = cmd.ExecuteReader();
                 if (reader.HasRows)
                 {
                     while(reader.Read())
                     {
                         Chore chore = new Chore();
                         chore.ChoreName = reader["ChoreName"].ToString();
-                        chore.ChoreId = int.Parse["ChoreId"].ToString();
+                        chore.ChoreId = int.Parse(reader["ChoreId"].ToString());
                         ChoreList.Add(chore);
                     }
                 }
