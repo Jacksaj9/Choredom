65438a06198063744c0a4f5d4fefba8d6df8f750
megarney <megarney0@gmail.com>
2023-04-25 20:58:04 -0500

    delete working - edit not

--- ChoredomUI/Pages/Chores/EditChore.cshtml.cs
+++ ChoredomUI/Pages/Chores/EditChore.cshtml.cs
@@ -1,12 +1,50 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ChoredomUI.Models;
+using Microsoft.Data.SqlClient;
 
 namespace ChoredomUI.Pages.Chores
 {
     public class EditChoreModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public Chore ExistingChore { get; set; } = new Chore();
+        public void OnGet(int id)
         {
+            using (SqlConnection conn = new SqlConnection(DBHelper.GetConnectionString()))
+            {
+                string sql = "SELECT * FROM Chore WHERE ChoreId = @choreId";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@choreId", id);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if(reader.HasRows)
+                {
+                    reader.Read();
+                    ExistingChore.ChoreName = reader["ChoreName"].ToString();
+                }
+            }
+        }
+
+        public IActionResult OnPost(int id)
+        {
+            if(ModelState.IsValid)
+            {
+                using (SqlConnection conn = new SqlConnection(DBHelper.GetConnectionString()))
+                {
+                    string sql = "UPDATE Chore SET ChoreName=@choreName WHERE ChoreId=@choreId";
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@choreName", ExistingChore.ChoreName);
+                    cmd.Parameters.AddWithValue("@choreId", id);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    return RedirectToPage("ChoreList");
+                }
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
