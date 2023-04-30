18f031f42b0e93bef04b06695c901700059c599f
megarney <megarney0@gmail.com>
2023-04-29 19:10:38 -0500

    dropdowns for locations and people in add chore

--- ChoredomUI/Pages/Chores/AddChore.cshtml.cs
+++ ChoredomUI/Pages/Chores/AddChore.cshtml.cs
@@ -11,16 +11,14 @@ namespace ChoredomUI.Pages.Chores
     {
         [BindProperty]
         public Chore NewChore { get; set; } = new Chore();
-        public void OnGet()
-        {
-        }
+        
         public IActionResult OnPost()
         {
             if (ModelState.IsValid)
             {
                 using (SqlConnection conn = new SqlConnection(DBHelper.GetConnectionString()))
                 {
-                    string sql = "INSERT INTO Chore(ChoreName)" + "VALUES (@choreName)";
+                    string sql = "INSERT INTO Chore(ChoreName, ChoreRooms)" + "VALUES (@choreName, @choreRooms)";
 
                     SqlCommand cmd = new SqlCommand(sql, conn);
 
@@ -37,5 +35,33 @@ namespace ChoredomUI.Pages.Chores
                 return Page();
             }
         }
+
+        [BindProperty]
+        public List<Person> PeopleList { get; set; } = new List<Person>();
+        public void OnGet()
+        {
+            using (SqlConnection conn = new SqlConnection(DBHelper.GetConnectionString()))
+            {
+                string sql = "SELECT * FROM Person Order By FirstName";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Person person = new Person();
+                        person.PersonId = int.Parse(reader["PersonId"].ToString());
+                        person.FirstName = reader["FirstName"].ToString();
+                        person.LastName = reader["LastName"].ToString();
+                        person.PersonBio = reader["PersonBio"].ToString();
+
+
+                        PeopleList.Add(person);
+                    }
+                }
+            }
+        }
+
     }
 }
